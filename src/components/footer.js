0d49f9cb1c4fbd9c603fa28e6c3f7697863aebf3
ShaheerDev <64355920+ShaheerDev@users.noreply.github.com>
2020-10-28 16:03:17 +0500

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+export default class Footer extends React.Component {
+  render(){
+    return(<div style={{position: "absolute", top:320 , left: 10}}>
+      <h5> &copy; ShaheerDev 2020. This is made for an assignment</h5>
+    </div>)
+  }
+}
