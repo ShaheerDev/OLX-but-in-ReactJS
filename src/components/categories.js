6982fa8c5cdb52adf9d5b096e35789e73ae991f6
ShaheerDev <64355920+ShaheerDev@users.noreply.github.com>
2020-10-31 10:26:43 -0700

    Update categories.js

--- src/components/categories.js
+++ src/components/categories.js
@@ -7,11 +7,9 @@ export default class Categories extends React.Component {
       <div class="topnav">
       <a href="#Mobile Phones" value="Mobile Phones">Mobile Phones</a>
       <a href="#Electronics" value="Electronics">Electronics</a>
-      <a href="#Cars" value="Cars">Cars</a>
-      <a href="#Motorcycles" value="Motorcycles">Motorcycles</a>
       <a href="#Vehicles" value="Vehicles">Vehicles</a>
+      <a href="#Motorcycles" value="Motorcycles">Motorcycles</a>
       <a href="#Houses" value="Houses">Houses</a>
-      <a href="#Animals" value="Animals">Animals</a>
       <a href="#Furniture and Home Decour" value="Furniture and Home Decour">Furniture and Home Decour</a>
       <a href="#Agricultural" value="Agricultural">Agricultural</a>
       <a  href="#Business" value="Businessl">Business</a>
