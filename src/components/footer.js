e46ebd57ef87539aff4b5f85abff62f809234700
ShaheerDev <64355920+ShaheerDev@users.noreply.github.com>
2020-11-02 13:55:40 +0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,10 @@ import '../App.css';
 
 export default class Footer extends React.Component {
   render(){
-    return(<div style={{position: "absolute", top:320 , left: 10}}>
-      <h5> &copy; ShaheerDev 2020. This is made for an assignment</h5>
-    </div>)
+    return(<>
+    <div style={{position: "absolute", top:320 , left: 10}}>
+    <h5> &copy; <a href="https://github.com/ShaheerDev">ShaheerDev</a> 2020, This is made for an assignment.</h5>
+    </div>
+    </>)
   }
 }
